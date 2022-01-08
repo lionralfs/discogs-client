969d01c6bde3337a9b1adc5b2770d9974f831ecf
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-08 18:39:28 +0100

    convert to ESM and use ES6 classes

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var discogs = (module.exports = {});
  * Expose Discogs utility function library
  */
 
-discogs.util = require('./lib/util.js');
+export * from './lib/util.js';
 
 /**
  * Expose Discogs Client class
  */
 
-discogs.Client = require('./lib/client.js');
+export default './lib/client.js';
