69857e3441912b208329ee106b4c71150941e212
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-08 22:19:43 +0100

    fix util test

--- test/util.js
+++ test/util.js
@@ -1,38 +1,25 @@
-import { log, assert, test as _test } from 'wru';
+import test from 'ava';
 import { stripVariation, escape, addParams } from '../lib/util.js';
 
-var tests = (module.exports = [
-    {
-        name: 'Util: Test stripVariation()',
-        test: function () {
-            var stripped = stripVariation('Artist (2)');
-            log('Artist name "Artist (2)" becomes: ' + stripped);
-            assert('Strip artist variation', stripped === 'Artist');
-        },
-    },
-    {
-        name: 'Util: Test escape()',
-        test: function () {
-            var escaped = escape('!@#$%^&*()+');
-            log('Escaped string: ' + escaped);
-            assert('Escape string "!@#$%^&*()+"', escaped === '!%40%23%24%25%5E%26*()%2B');
-        },
-    },
-    {
-        name: 'Util: Test addParams()',
-        test: function () {
-            assert(
-                'URL with no query string',
-                addParams('http://an-url.com', { foo: 'bar', baz: 1 }) === 'http://an-url.com?foo=bar&baz=1'
-            );
-            assert(
-                'URL with existing query string',
-                addParams('http://an-url.com?y=5', { foo: 'bar', baz: 1 }) === 'http://an-url.com?y=5&foo=bar&baz=1'
-            );
-        },
-    },
-]);
+test('Util: Test stripVariation()', t => {
+    let stripped = stripVariation('Artist (2)');
+    t.is(stripped, 'Artist', 'Strip artist variation');
+});
 
-if (!module.parent) {
-    _test(tests);
-}
+test('Util: Test escape()', t => {
+    let escaped = escape('!@#$%^&*()+');
+    t.is(escaped, '!%40%23%24%25%5E%26*()%2B', 'Escape string "!@#$%^&*()+"');
+});
+
+test('Util: Test addParams()', t => {
+    t.is(
+        addParams('http://an-url.com', { foo: 'bar', baz: 1 }),
+        'http://an-url.com?foo=bar&baz=1',
+        'URL with no query string'
+    );
+    t.is(
+        addParams('http://an-url.com?y=5', { foo: 'bar', baz: 1 }),
+        'http://an-url.com?y=5&foo=bar&baz=1',
+        'URL with existing query string'
+    );
+});
