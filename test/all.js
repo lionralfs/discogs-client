b64592a3ce0a33291c2826c52e80afafa3fd628d
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-08 21:25:42 +0100

    delete old test grouping file

--- test/all.js
+++
@@ -1,9 +0,0 @@
-import { test } from 'wru';
-var tests = [];
-var files = ['error', 'queue', 'util', 'client', 'database'];
-
-for (var i in files) {
-    tests = tests.concat(require('./' + files[i] + '.js'));
-}
-
-test(tests);
