12a095e485e88624f1a9e7dff6e874f36f7ea7da
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-08 21:30:36 +0100

    fix error tests and error implementation

--- test/error.js
+++ test/error.js
@@ -1,27 +1,16 @@
-import { assert, test as _test } from 'wru';
+import test from 'ava';
 import { DiscogsError, AuthError } from '../lib/error.js';
 
-var tests = (module.exports = [
-    {
-        name: 'Error: Test DiscogsError',
-        test: function () {
-            var discogsError = new DiscogsError(403, 'Test');
-            assert('Instance of DiscogsError', discogsError instanceof DiscogsError);
-            assert('Instance of Error', discogsError instanceof Error);
-            assert('Status code === 403', discogsError.statusCode === 403);
-        },
-    },
-    {
-        name: 'Error: Test AuthError',
-        test: function () {
-            var authError = new AuthError();
-            assert('Instance of AuthError', authError instanceof AuthError);
-            assert('Instance of Error', authError instanceof Error);
-            assert('Status code === 401', authError.statusCode === 401);
-        },
-    },
-]);
+test('Error: Test DiscogsError', t => {
+    let discogsError = new DiscogsError(403, 'Test');
+    t.true(discogsError instanceof DiscogsError, 'Instance of DiscogsError');
+    t.true(discogsError instanceof Error, 'Instance of Error');
+    t.is(discogsError.statusCode, 403, 'Status code === 403');
+});
 
-if (!module.parent) {
-    _test(tests);
-}
+test('Error: Test AuthError', t => {
+    let authError = new AuthError();
+    t.true(authError instanceof AuthError, 'Instance of AuthError');
+    t.true(authError instanceof Error, 'Instance of Error');
+    t.is(authError.statusCode, 401, 'Status code === 401');
+});
