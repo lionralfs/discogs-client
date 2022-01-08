f1875437e977eaf30807799cd68459dbcd7d69bb
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-08 21:24:29 +0100

    start rewriting tests with ava & msw

--- test/client.js
+++ test/client.js
@@ -1,69 +1,55 @@
-import { assert, async, test as _test } from 'wru';
-import nock, { cleanAll } from 'nock';
-import DiscogsClient from '../lib/client.js';
+import test from 'ava';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node/lib/index.js';
+import { DiscogsClient } from '../lib/client.js';
 
-var tests = (module.exports = [
-    {
-        name: 'DiscogsClient: Test instance',
-        test: function () {
-            assert('Instance of DiscogsClient', new DiscogsClient() instanceof DiscogsClient);
-        },
-    },
-    {
-        name: 'DiscogsClient: Test authenticated()',
-        test: function () {
-            assert('Authentication level 1 === false', new DiscogsClient().authenticated(1) === false);
-        },
-    },
-    {
-        name: 'DiscogsClient: Test get()',
-        test: function () {
-            var client = new DiscogsClient();
-            client.get(
-                { url: '/labels/1' },
-                async(function (err, data) {
-                    assert('No error', !err);
-                    assert('Correct response data', data && data.id === 1);
-                })
-            );
-        },
-    },
-    {
-        name: 'DiscogsClient: Test Promise',
-        test: function () {
-            var client = new DiscogsClient();
-            var promise = client.about();
-            var isPromise = typeof promise.then === 'function';
-            assert('Returns Promise', isPromise);
-            if (isPromise) {
-                promise.then(
-                    async(function (data) {
-                        assert('Promis resolved', typeof data.disconnect !== 'undefined');
-                    })
-                );
-            }
-        },
-    },
-    {
-        name: 'DiscogsClient: Test custom configuration',
-        test: function () {
-            nock('https://www.example.com').get('/labels/1').reply(200, '{"result": "success"}');
+const server = setupServer(
+    rest.get('https://api.discogs.com/labels/1', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json({ result: 'success', id: 1 }));
+    }),
+    rest.get('https://api.discogs.com/', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json({}));
+    }),
+    rest.get('https://www.example.com/labels/1', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json({ result: 'success' }));
+    })
+);
 
-            var client = new DiscogsClient().setConfig({ host: 'www.example.com' });
-            client.get(
-                { url: '/labels/1' },
-                async(function (err, data) {
-                    assert('No error', !err);
-                    assert('Correct response data', data && data.result === 'success');
-                })
-            );
-        },
-        teardown: function () {
-            cleanAll();
-        },
-    },
-]);
+// Enable API mocking before tests.
+test.before(() => server.listen());
 
-if (!module.parent) {
-    _test(tests);
-}
+// Reset any runtime request handlers we may add during the tests.
+test.afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+test.after(() => server.close());
+
+test('DiscogsClient: Test instance', t => {
+    t.true(new DiscogsClient() instanceof DiscogsClient);
+});
+
+test('DiscogsClient: Test authenticated()', t => {
+    t.false(new DiscogsClient().authenticated(1), 'Authentication level 1 === false');
+});
+
+test('DiscogsClient: Test get()', async t => {
+    let client = new DiscogsClient();
+    let data = await client.get({ url: '/labels/1' });
+    t.is(data?.id, 1, 'Correct response data');
+});
+
+test('DiscogsClient: Test Promise', async t => {
+    let client = new DiscogsClient();
+    let promise = client.about();
+    t.is(typeof promise.then, 'function');
+
+    let data = await promise;
+    t.not(typeof data.disconnect, 'undefined', 'Promise resolved');
+});
+
+test('DiscogsClient: Test custom configuration', async t => {
+    let client = new DiscogsClient().setConfig({ host: 'www.example.com' });
+    let data = await client.get({ url: '/labels/1' });
+
+    t.is(data?.result, 'success', 'Correct response data');
+});
