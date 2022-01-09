8665bb53d03982a4457d975a9aac0b27c073a598
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-09 22:20:15 +0100

    test: user contributions

--- test/user.js
+++ test/user.js
@@ -78,3 +78,25 @@ test.serial('User: Get User Submissions', async t => {
     let client = new DiscogsClient('agent', { userToken: 'test-token' });
     await client.user().getSubmissions('rodneyfool', { page: 4, per_page: 48 });
 });
+
+test.serial('User: Get User Contributions', async t => {
+    t.plan(1);
+
+    server.use(
+        rest.get('https://api.discogs.com/users/rodneyfool/contributions', (req, res, ctx) => {
+            t.deepEqual(
+                [...req.url.searchParams.entries()],
+                [
+                    ['page', '2'],
+                    ['per_page', '50'],
+                    ['sort', 'artist'],
+                    ['sort_order', 'desc'],
+                ]
+            );
+            return res(ctx.status(200), ctx.json({}));
+        })
+    );
+
+    let client = new DiscogsClient('agent', { userToken: 'test-token' });
+    await client.user().getContributions('rodneyfool', { page: 2, per_page: 50, sort: 'artist', sort_order: 'desc' });
+});
