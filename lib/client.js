bartve/disconnect

Discogs Auth implemented

lib/client.js
@@ -3,30 +3,12 @@
 var https = require('https'),
 	zlib = require('zlib'),
 	url = require('url'),
-	queryString = require('querystring'),
-	OAuth = require('oauth-1.0a'),
 	pkg = require('../package.json'),
 	error = require('./error.js'),
-	queue = require('./queue.js');
+	queue = require('./queue.js'),
+	util = require('./util.js');
 
 module.exports = DiscogsClient;
-
-/**
- * Deep merge two objects and return the result
- * @param {object} target - The target object (by reference!)
- * @param {object} source - The source object
- */
-
-var merge = function(target, source){
-	for(var key in source){
-		if(source[key] && (typeof source[key] === 'object')){
-			target[key] = merge((Array.isArray(source[key]) ? [] : {}), source[key]);
-		}else{
-			target[key] = source[key];
-		}
-	}
-	return target;
-};
 
 /**
  * Default configuration
@@ -34,35 +16,44 @@ var merge = function(target, source){
 
 var defaultConfig = {
 	host: 'api.discogs.com',
-	oauthRequestUrl: 'https://api.discogs.com/oauth/request_token',
-	oauthAccessUrl: 'https://api.discogs.com/oauth/access_token',
-	oauthAuthorizeUrl: 'https://www.discogs.com/oauth/authorize',
 	port: 443,
-	customHeaders: {
-		'Accept': 'application/json; application/octet-stream',
-		'Accept-Encoding': 'gzip,deflate',
-		'User-Agent': 'DisConnectClient/'+pkg.version+' +'+pkg.homepage
-	}
+	userAgent: 'DisConnectClient/'+pkg.version+' +'+pkg.homepage
 };
 
 /**
  * Object constructor
  * @param {string} [userAgent] - The name of the user agent to use to make API calls
- * @param {object} [oauth] - Optional OAuth data object
+ * @param {object} [auth] - Optional authorization data object
  * @returns {DiscogsClient}
  */
 
-function DiscogsClient(userAgent, oauth){
+function DiscogsClient(userAgent, auth){
 	// Allow the class to be called as a function, returning an instance
 	if(!(this instanceof DiscogsClient)){
-        return new DiscogsClient(userAgent, oauth);
+        return new DiscogsClient(userAgent, auth);
 	}
-	// Set the custom UserAgent when provided
-	if(typeof userAgent === 'string'){ this.userAgent = userAgent; }
+	// Set the default configuration
+	this.config = util.merge({}, defaultConfig);
+	// Set the custom User Agent when provided
+	if(typeof userAgent === 'string'){
+		this.config.userAgent = userAgent;
+	}
 	// No userAgent provided, but instead we have an accessObject
-	if((arguments.length === 1) && (typeof userAgent === 'object')){ oauth = userAgent; }
-	// Set OAuth data when provided by making a shallow copy of the oauth parameter
-	this.oauth = (typeof oauth === 'object') ? merge({}, oauth) : {version: '1.0', signatureMethod: 'HMAC-SHA1', status: null};
+	if((arguments.length === 1) && (typeof userAgent === 'object')){ auth = userAgent; }
+	// Set auth data when provided
+	if(auth && (typeof auth === 'object')){
+		if(!auth.hasOwnProperty('method')){
+			auth.method = 'discogs';
+		}
+		if(!auth.hasOwnProperty('level')){
+			if(auth.userToken){
+				auth.level = 2;
+			}else if(auth.consumerKey && auth.consumerSecret){
+				auth.level = 1;
+			}
+		}
+		this.auth = util.merge({}, auth);
+	}
 }
 
 /**
@@ -71,74 +62,28 @@ function DiscogsClient(userAgent, oauth){
  * @returns {DiscogsClient}
  */
 DiscogsClient.prototype.setConfig = function(customConfig){
-	this.customConfig = merge(merge({}, defaultConfig), customConfig);
+	util.merge(this.config, customConfig);
 	return this;
 };
 
 /**
- * Get an OAuth request token from Discogs
- * @param {string} consumerKey - The Discogs consumer key
- * @param {string} consumerSecret - The Discogs consumer secret
- * @param {string} callbackUrl - The url for redirection after obtaining the request token
- * @param {function} [callback] - Callback function receiving the data
- * @returns {DiscogsClient}
- */
- 
-DiscogsClient.prototype.getRequestToken = function(consumerKey, consumerSecret, callbackUrl, callback){
-	var oauth = this.oauth, config = (this.customConfig||defaultConfig), oa = new OAuth({consumer: {public: consumerKey, secret: consumerSecret}});
-	oauth.consumerKey = consumerKey;
-	oauth.consumerSecret = consumerSecret;
-	this._rawRequest({url: config.oauthRequestUrl+'?oauth_callback='+oa.percentEncode(callbackUrl)}, function(err, data){
-		if(!err && data){
-			data = queryString.parse(data);
-			oauth.token = data.oauth_token;
-			oauth.tokenSecret = data.oauth_token_secret;
-			oauth.authorizeUrl = config.oauthAuthorizeUrl+'?oauth_token='+data.oauth_token;
-			oauth.status = 'request';
-		}
-		if(typeof callback === 'function'){ callback(err, oauth); }
-	});
-	return this;
-};
-
-/**
- * Get an OAuth access token from Discogs
- * @param {string} verifier - The OAuth 1.0a verification code returned by Discogs
- * @param {function} [callback] - Callback function receiving the data
- * @returns {DiscogsClient}
- */
- 
-DiscogsClient.prototype.getAccessToken = function(verifier, callback){
-	var oauth = this.oauth, oa = new OAuth({consumer: {public: oauth.consumerKey, secret: oauth.consumerSecret}});
-	this._rawRequest({url: (this.customConfig||defaultConfig).oauthAccessUrl+'?oauth_verifier='+oa.percentEncode(verifier)}, function(err, data){
-		if(!err && data){
-			data = queryString.parse(data);
-			oauth.token = data.oauth_token;
-			oauth.tokenSecret = data.oauth_token_secret;
-			oauth.status = 'access';
-			delete oauth.authorizeUrl;
-		}
-		if(typeof callback === 'function'){ callback(err, oauth); }
-	});
-	return this;
-};
-
-/**
- * Return whether OAuth authentication is complete i.e. there is a valis access token
+ * Return whether the client is authenticated for the optionally given access level
+ * @param {number} [level] - Optional authentication level
  * @returns {boolean}
  */
 
-DiscogsClient.prototype.authenticated = function(){
-	return (this.oauth.status === 'access');
+DiscogsClient.prototype.authenticated = function(level){
+	level = level||0;
+	return ((level > 0) && this.auth && (this.auth.level >= level));
 };
 
 /**
- * Test OAuth authentication by getting the identity resource for the authenticated user
+ * Test authentication by getting the identity resource for the authenticated user
  * @param {function} callback - Callback function receiving the data
  */
 
 DiscogsClient.prototype.identity = function(callback){
-   this.get({url:'/oauth/identity', requireAuth: true}, callback);
+   this.get({url: '/oauth/identity', authLevel: 2}, callback);
 };
 
 /**
@@ -149,7 +94,14 @@ DiscogsClient.prototype.identity = function(callback){
 DiscogsClient.prototype.about = function(callback){
 	var self = this;
 	this.get('', function(err, data){
-		if(data){ data.disconnect = { version: pkg.version, userAgent: (self.userAgent||(self.customConfig||defaultConfig).customHeaders['User-Agent']) }; }
+		if(data){ 
+			data.disconnect = {
+				version: pkg.version, 
+				userAgent: self.config.userAgent,
+				authMethod: (self.auth ? self.auth.method : 'none'),
+				authLevel: (self.auth ? self.auth.level : 0)
+			};
+		}
 		callback(err, data);
 	}); 
 };
@@ -170,18 +122,17 @@ DiscogsClient.prototype._rawRequest = function(options, callback){
 	var data = options.data||null,
 		method = options.method||'GET',
 		urlParts = url.parse(options.url),
-		encoding = options.encoding||'utf8',
-		config = (this.customConfig||defaultConfig);
+		encoding = options.encoding||'utf8';
 	
 	// Build request headers
 	var headers = {
-		'Host': config.host,
+		'User-Agent': this.config.userAgent,
+		'Accept': 'application/json; application/octet-stream',
+		'Accept-Encoding': 'gzip,deflate',
+		'Host': this.config.host,
 		'Connection': 'close',
 		'Content-Length': 0
 	};
-	headers = merge(headers, config.customHeaders);
-	// Set custom user agent when present
-	if(this.userAgent){ headers['User-Agent'] = this.userAgent; }
 	
 	// Add content headers for POST/PUT requests that contain data
 	if(data){
@@ -190,18 +141,28 @@ DiscogsClient.prototype._rawRequest = function(options, callback){
 		headers['Content-Length'] = Buffer.byteLength(data, 'utf8');
 	}
 	
-	// Add Authorization header when authenticated or in the process of authenticating
-	if(this.oauth.consumerKey){
-		var oa = new OAuth({consumer: {public: this.oauth.consumerKey, secret: this.oauth.consumerSecret}}),
-			fullUrl = (urlParts.protocol && urlParts.host) ? urlParts.href : 'https://'+config.host+urlParts.path,
-			authObj = oa.authorize({method: method, url: fullUrl}, {public: this.oauth.token, secret: this.oauth.tokenSecret});
-		headers['Authorization'] = oa.toHeader(authObj).Authorization;
+	// Add Authorization header when authenticated (or in the process of authenticating)
+	if(this.auth && (this.auth.consumerKey || this.auth.userToken)){
+		var authHeader = '';
+		if(this.auth.method === 'oauth'){
+			var OAuth = require('./oauth.js'),
+				fullUrl = (urlParts.protocol && urlParts.host) ? urlParts.href : 'https://'+this.config.host+urlParts.path;
+			authHeader = new OAuth(this.auth).toHeader(method, fullUrl);
+		}else if(this.auth.method === 'discogs'){
+			authHeader = 'Discogs';
+			if(this.auth.userToken){
+				authHeader += ' token='+this.auth.userToken;
+			}else if(this.auth.consumerKey){
+				authHeader += ' key='+this.auth.consumerKey+', secret='+this.auth.consumerSecret;
+			}
+		}
+		headers['Authorization'] = authHeader;
 	}
 	
 	// Set the HTTPS request options
 	var options = {
-		host: urlParts.host||config.host,
-		port: urlParts.port||config.port,
+		host: urlParts.host||this.config.host,
+		port: urlParts.port||this.config.port,
 		path: urlParts.path,
 		method: method,
 		headers: headers
@@ -274,21 +235,36 @@ DiscogsClient.prototype._rawRequest = function(options, callback){
  */
 
 DiscogsClient.prototype._request = function(options, callback){
-	var self = this, hasCb = (typeof callback === 'function');
+	var self = this, hasCb = (typeof callback === 'function'),
+		doRequest = function(){
+			self._rawRequest(options, function(err, data, rateLimit){
+				if(data && ((typeof options === 'string') || options.json)){
+					data = JSON.parse(data);
+				}
+				if(hasCb){ callback(err, data, rateLimit); }
+			});
+		};
+	// By default, queue requests
+	if(!options.hasOwnProperty('queue')){
+		options.queue = true;
+	}
+	// By default, expect responses to be JSON
+	if(!options.hasOwnProperty('json')){
+		options.json = true;
+	}
 	// Check whether authentication is required
-	if(!options.requireAuth || this.authenticated()){
-		queue.add(function(err){ // Add API request to the execution queue
-			if(!err){
-				self._rawRequest(options, function(err, data, rateLimit){
-					if(data && ((typeof options === 'string') || (options.encoding !== 'binary'))){
-						data = JSON.parse(data);
-					}
-					if(hasCb){ callback(err, data, rateLimit); }
-				});
-			}else{ // Can't add to the queue because it's full
-				if(hasCb){ callback(err); }
-			}
-		});
+	if(!options.authLevel || this.authenticated(options.authLevel)){
+		if(options.queue){ // Add API request to the execution queue
+			queue.add(function(err){
+				if(!err){
+					doRequest();
+				}else{ // Can't add to the queue because it's full
+					if(hasCb){ callback(err); }
+				}
+			});
+		}else{ // Don't queue, just do the request
+			doRequest();
+		}
 	}else{
 		if(hasCb){ callback(new error.AuthError()); }
 	}
@@ -317,7 +293,7 @@ DiscogsClient.prototype.get = function(options, callback){
 
 DiscogsClient.prototype.post = function(options, data, callback){
 	if(typeof options === 'string'){ options = {url: options}; }
-	return this._request(merge(options, {method: 'POST', data: data}), callback);
+	return this._request(util.merge(options, {method: 'POST', data: data}), callback);
 };
 
 /**
@@ -330,7 +306,7 @@ DiscogsClient.prototype.post = function(options, data, callback){
 
 DiscogsClient.prototype.put = function(options, data, callback){
 	if(typeof options === 'string'){ options = {url: options}; }
-	return this._request(merge(options, {method: 'PUT', data: data}), callback);
+	return this._request(util.merge(options, {method: 'PUT', data: data}), callback);
 };
 
 /**
@@ -342,7 +318,17 @@ DiscogsClient.prototype.put = function(options, data, callback){
 
 DiscogsClient.prototype.delete = function(options, callback){
 	if(typeof options === 'string'){ options = {url: options}; }
-	return this._request(merge(options, {method: 'DELETE'}), callback);
+	return this._request(util.merge(options, {method: 'DELETE'}), callback);
+};
+
+/**
+ * Get an instance of the Discogs OAuth class
+ * @returns {DiscogsOAuth}
+ */
+
+DiscogsClient.prototype.oauth = function(){
+	var OAuth = require('./oauth.js');
+	return new OAuth(this.auth);
 };
 
 /**

lionralfs/discogs-client

lib/client.js
@@ -3,30 +3,12 @@
 var https = require('https'),
     zlib = require('zlib'),
     url = require('url'),
-    queryString = require('querystring'),
-    OAuth = require('oauth-1.0a'),
     pkg = require('../package.json'),
     error = require('./error.js'),
-    queue = require('./queue.js');
+    queue = require('./queue.js'),
+    util = require('./util.js');
 
 module.exports = DiscogsClient;
-
-/**
- * Deep merge two objects and return the result
- * @param {object} target - The target object (by reference!)
- * @param {object} source - The source object
- */
-
-var merge = function (target, source) {
-    for (var key in source) {
-        if (source[key] && typeof source[key] === 'object') {
-            target[key] = merge(Array.isArray(source[key]) ? [] : {}, source[key]);
-        } else {
-            target[key] = source[key];
-        }
-    }
-    return target;
-};
 
 /**
  * Default configuration
@@ -34,40 +16,46 @@ var merge = function (target, source) {
 
 var defaultConfig = {
     host: 'api.discogs.com',
-    oauthRequestUrl: 'https://api.discogs.com/oauth/request_token',
-    oauthAccessUrl: 'https://api.discogs.com/oauth/access_token',
-    oauthAuthorizeUrl: 'https://www.discogs.com/oauth/authorize',
     port: 443,
-    customHeaders: {
-        Accept: 'application/json; application/octet-stream',
-        'Accept-Encoding': 'gzip,deflate',
-        'User-Agent': 'DisConnectClient/' + pkg.version + ' +' + pkg.homepage,
-    },
+    userAgent: 'DisConnectClient/' + pkg.version + ' +' + pkg.homepage,
 };
 
 /**
  * Object constructor
  * @param {string} [userAgent] - The name of the user agent to use to make API calls
- * @param {object} [oauth] - Optional OAuth data object
+ * @param {object} [auth] - Optional authorization data object
  * @returns {DiscogsClient}
  */
 
-function DiscogsClient(userAgent, oauth) {
+function DiscogsClient(userAgent, auth) {
     // Allow the class to be called as a function, returning an instance
     if (!(this instanceof DiscogsClient)) {
-        return new DiscogsClient(userAgent, oauth);
+        return new DiscogsClient(userAgent, auth);
     }
-    // Set the custom UserAgent when provided
+    // Set the default configuration
+    this.config = util.merge({}, defaultConfig);
+    // Set the custom User Agent when provided
     if (typeof userAgent === 'string') {
-        this.userAgent = userAgent;
+        this.config.userAgent = userAgent;
     }
     // No userAgent provided, but instead we have an accessObject
     if (arguments.length === 1 && typeof userAgent === 'object') {
-        oauth = userAgent;
+        auth = userAgent;
     }
-    // Set OAuth data when provided by making a shallow copy of the oauth parameter
-    this.oauth =
-        typeof oauth === 'object' ? merge({}, oauth) : { version: '1.0', signatureMethod: 'HMAC-SHA1', status: null };
+    // Set auth data when provided
+    if (auth && typeof auth === 'object') {
+        if (!auth.hasOwnProperty('method')) {
+            auth.method = 'discogs';
+        }
+        if (!auth.hasOwnProperty('level')) {
+            if (auth.userToken) {
+                auth.level = 2;
+            } else if (auth.consumerKey && auth.consumerSecret) {
+                auth.level = 1;
+            }
+        }
+        this.auth = util.merge({}, auth);
+    }
 }
 
 /**
@@ -76,87 +64,28 @@ function DiscogsClient(userAgent, oauth) {
  * @returns {DiscogsClient}
  */
 DiscogsClient.prototype.setConfig = function (customConfig) {
-    this.customConfig = merge(merge({}, defaultConfig), customConfig);
+    util.merge(this.config, customConfig);
     return this;
 };
 
 /**
- * Get an OAuth request token from Discogs
- * @param {string} consumerKey - The Discogs consumer key
- * @param {string} consumerSecret - The Discogs consumer secret
- * @param {string} callbackUrl - The url for redirection after obtaining the request token
- * @param {function} [callback] - Callback function receiving the data
- * @returns {DiscogsClient}
- */
-
-DiscogsClient.prototype.getRequestToken = function (consumerKey, consumerSecret, callbackUrl, callback) {
-    var oauth = this.oauth,
-        config = this.customConfig || defaultConfig,
-        oa = new OAuth({ consumer: { public: consumerKey, secret: consumerSecret } });
-    oauth.consumerKey = consumerKey;
-    oauth.consumerSecret = consumerSecret;
-    this._rawRequest(
-        { url: config.oauthRequestUrl + '?oauth_callback=' + oa.percentEncode(callbackUrl) },
-        function (err, data) {
-            if (!err && data) {
-                data = queryString.parse(data);
-                oauth.token = data.oauth_token;
-                oauth.tokenSecret = data.oauth_token_secret;
-                oauth.authorizeUrl = config.oauthAuthorizeUrl + '?oauth_token=' + data.oauth_token;
-                oauth.status = 'request';
-            }
-            if (typeof callback === 'function') {
-                callback(err, oauth);
-            }
-        }
-    );
-    return this;
-};
-
-/**
- * Get an OAuth access token from Discogs
- * @param {string} verifier - The OAuth 1.0a verification code returned by Discogs
- * @param {function} [callback] - Callback function receiving the data
- * @returns {DiscogsClient}
- */
-
-DiscogsClient.prototype.getAccessToken = function (verifier, callback) {
-    var oauth = this.oauth,
-        oa = new OAuth({ consumer: { public: oauth.consumerKey, secret: oauth.consumerSecret } });
-    this._rawRequest(
-        { url: (this.customConfig || defaultConfig).oauthAccessUrl + '?oauth_verifier=' + oa.percentEncode(verifier) },
-        function (err, data) {
-            if (!err && data) {
-                data = queryString.parse(data);
-                oauth.token = data.oauth_token;
-                oauth.tokenSecret = data.oauth_token_secret;
-                oauth.status = 'access';
-                delete oauth.authorizeUrl;
-            }
-            if (typeof callback === 'function') {
-                callback(err, oauth);
-            }
-        }
-    );
-    return this;
-};
-
-/**
- * Return whether OAuth authentication is complete i.e. there is a valis access token
+ * Return whether the client is authenticated for the optionally given access level
+ * @param {number} [level] - Optional authentication level
  * @returns {boolean}
  */
 
-DiscogsClient.prototype.authenticated = function () {
-    return this.oauth.status === 'access';
+DiscogsClient.prototype.authenticated = function (level) {
+    level = level || 0;
+    return level > 0 && this.auth && this.auth.level >= level;
 };
 
 /**
- * Test OAuth authentication by getting the identity resource for the authenticated user
+ * Test authentication by getting the identity resource for the authenticated user
  * @param {function} callback - Callback function receiving the data
  */
 
 DiscogsClient.prototype.identity = function (callback) {
-    this.get({ url: '/oauth/identity', requireAuth: true }, callback);
+    this.get({ url: '/oauth/identity', authLevel: 2 }, callback);
 };
 
 /**
@@ -170,7 +99,9 @@ DiscogsClient.prototype.about = function (callback) {
         if (data) {
             data.disconnect = {
                 version: pkg.version,
-                userAgent: self.userAgent || (self.customConfig || defaultConfig).customHeaders['User-Agent'],
+                userAgent: self.config.userAgent,
+                authMethod: self.auth ? self.auth.method : 'none',
+                authLevel: self.auth ? self.auth.level : 0,
             };
         }
         callback(err, data);
@@ -193,20 +124,17 @@ DiscogsClient.prototype._rawRequest = function (options, callback) {
     var data = options.data || null,
         method = options.method || 'GET',
         urlParts = url.parse(options.url),
-        encoding = options.encoding || 'utf8',
-        config = this.customConfig || defaultConfig;
+        encoding = options.encoding || 'utf8';
 
     // Build request headers
     var headers = {
-        Host: config.host,
+        'User-Agent': this.config.userAgent,
+        Accept: 'application/json; application/octet-stream',
+        'Accept-Encoding': 'gzip,deflate',
+        Host: this.config.host,
         Connection: 'close',
         'Content-Length': 0,
     };
-    headers = merge(headers, config.customHeaders);
-    // Set custom user agent when present
-    if (this.userAgent) {
-        headers['User-Agent'] = this.userAgent;
-    }
 
     // Add content headers for POST/PUT requests that contain data
     if (data) {
@@ -217,21 +145,29 @@ DiscogsClient.prototype._rawRequest = function (options, callback) {
         headers['Content-Length'] = Buffer.byteLength(data, 'utf8');
     }
 
-    // Add Authorization header when authenticated or in the process of authenticating
-    if (this.oauth.consumerKey) {
-        var oa = new OAuth({ consumer: { public: this.oauth.consumerKey, secret: this.oauth.consumerSecret } }),
-            fullUrl = urlParts.protocol && urlParts.host ? urlParts.href : 'https://' + config.host + urlParts.path,
-            authObj = oa.authorize(
-                { method: method, url: fullUrl },
-                { public: this.oauth.token, secret: this.oauth.tokenSecret }
-            );
-        headers['Authorization'] = oa.toHeader(authObj).Authorization;
+    // Add Authorization header when authenticated (or in the process of authenticating)
+    if (this.auth && (this.auth.consumerKey || this.auth.userToken)) {
+        var authHeader = '';
+        if (this.auth.method === 'oauth') {
+            var OAuth = require('./oauth.js'),
+                fullUrl =
+                    urlParts.protocol && urlParts.host ? urlParts.href : 'https://' + this.config.host + urlParts.path;
+            authHeader = new OAuth(this.auth).toHeader(method, fullUrl);
+        } else if (this.auth.method === 'discogs') {
+            authHeader = 'Discogs';
+            if (this.auth.userToken) {
+                authHeader += ' token=' + this.auth.userToken;
+            } else if (this.auth.consumerKey) {
+                authHeader += ' key=' + this.auth.consumerKey + ', secret=' + this.auth.consumerSecret;
+            }
+        }
+        headers['Authorization'] = authHeader;
     }
 
     // Set the HTTPS request options
     var options = {
-        host: urlParts.host || config.host,
-        port: urlParts.port || config.port,
+        host: urlParts.host || this.config.host,
+        port: urlParts.port || this.config.port,
         path: urlParts.path,
         method: method,
         headers: headers,
@@ -315,27 +251,43 @@ DiscogsClient.prototype._rawRequest = function (options, callback) {
 
 DiscogsClient.prototype._request = function (options, callback) {
     var self = this,
-        hasCb = typeof callback === 'function';
-    // Check whether authentication is required
-    if (!options.requireAuth || this.authenticated()) {
-        queue.add(function (err) {
-            // Add API request to the execution queue
-            if (!err) {
-                self._rawRequest(options, function (err, data, rateLimit) {
-                    if (data && (typeof options === 'string' || options.encoding !== 'binary')) {
-                        data = JSON.parse(data);
-                    }
-                    if (hasCb) {
-                        callback(err, data, rateLimit);
-                    }
-                });
-            } else {
-                // Can't add to the queue because it's full
-                if (hasCb) {
-                    callback(err);
+        hasCb = typeof callback === 'function',
+        doRequest = function () {
+            self._rawRequest(options, function (err, data, rateLimit) {
+                if (data && (typeof options === 'string' || options.json)) {
+                    data = JSON.parse(data);
                 }
-            }
-        });
+                if (hasCb) {
+                    callback(err, data, rateLimit);
+                }
+            });
+        };
+    // By default, queue requests
+    if (!options.hasOwnProperty('queue')) {
+        options.queue = true;
+    }
+    // By default, expect responses to be JSON
+    if (!options.hasOwnProperty('json')) {
+        options.json = true;
+    }
+    // Check whether authentication is required
+    if (!options.authLevel || this.authenticated(options.authLevel)) {
+        if (options.queue) {
+            // Add API request to the execution queue
+            queue.add(function (err) {
+                if (!err) {
+                    doRequest();
+                } else {
+                    // Can't add to the queue because it's full
+                    if (hasCb) {
+                        callback(err);
+                    }
+                }
+            });
+        } else {
+            // Don't queue, just do the request
+            doRequest();
+        }
     } else {
         if (hasCb) {
             callback(new error.AuthError());
@@ -370,7 +322,7 @@ DiscogsClient.prototype.post = function (options, data, callback) {
     if (typeof options === 'string') {
         options = { url: options };
     }
-    return this._request(merge(options, { method: 'POST', data: data }), callback);
+    return this._request(util.merge(options, { method: 'POST', data: data }), callback);
 };
 
 /**
@@ -385,7 +337,7 @@ DiscogsClient.prototype.put = function (options, data, callback) {
     if (typeof options === 'string') {
         options = { url: options };
     }
-    return this._request(merge(options, { method: 'PUT', data: data }), callback);
+    return this._request(util.merge(options, { method: 'PUT', data: data }), callback);
 };
 
 /**
@@ -399,7 +351,17 @@ DiscogsClient.prototype.delete = function (options, callback) {
     if (typeof options === 'string') {
         options = { url: options };
     }
-    return this._request(merge(options, { method: 'DELETE' }), callback);
+    return this._request(util.merge(options, { method: 'DELETE' }), callback);
+};
+
+/**
+ * Get an instance of the Discogs OAuth class
+ * @returns {DiscogsOAuth}
+ */
+
+DiscogsClient.prototype.oauth = function () {
+    var OAuth = require('./oauth.js');
+    return new OAuth(this.auth);
 };
 
 /**