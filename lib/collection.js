bartve/disconnect

Initial commit. Earlier versions are not in Git.

lib/collection.js
@@ -1,0 +1,145 @@
+var util = require('./util.js');
+
+module.exports = function(client){
+	var collection = {};
+	
+	/**
+	 * Get metadata for a specified collection folder or list all collection folders
+	 * @param {String} user - The user name
+	 * @param {Integer} [folder] - An optional folder ID
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.folders = function(user, folder, callback){
+		var url = '/users/'+user+'/collection/folders';
+		if((arguments.length === 2) && (typeof folder === 'function')){
+			callback = folder;
+		}else{
+			url += '/'+folder;
+		}
+		client.get(url, callback);
+	};
+	
+	/**
+	 * Add a new collection folder
+	 * @param {String} user - The user name
+	 * @param {String} name - The folder name
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.addFolder = function(user, name, callback){
+		if(client.authenticated()){
+			client.post('/users/'+util.escape(user)+'/collection/folders', {name: name}, callback);
+		}else{
+			callback(new Error('You must be authenticated as the owner in order to add a new folder.'));
+		}
+	};
+	
+	/**
+	 * Change a folder name. The name of folder 0 and 1 can't be changed.
+	 * @param {String} user - The user name
+	 * @param {Integer}	folder - The folder ID
+	 * @param {String} name - The new folder name
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.editFolder = function(user, folder, name, callback){
+		if(client.authenticated()){
+			client.post('/users/'+util.escape(user)+'/collection/folders/'+folder, {name: name}, callback);
+		}else{
+			callback(new Error('You must be authenticated as the owner in order to edit a folder name.'));
+		}
+	};
+	
+	/**
+	 * Delete a folder. A folder must be empty before it can be deleted.
+	 * @param {String} user - The user name
+	 * @param {Integer}	folder - The folder ID
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.deleteFolder = function(user, folder, callback){
+		if(client.authenticated()){
+			client.delete('/users/'+util.escape(user)+'/collection/folders/'+folder, callback);
+		}else{
+			callback(new Error('You must be authenticated as the owner in order to delete a folder.'));
+		}
+	};
+	
+	/**
+	 * Get the releases in a user's collection folder
+	 * http://www.discogs.com/developers/resources/user/collection.html#list-releases-in-folder
+	 * @param {String} user - The user name
+	 * @param {Integer} folder - The folder ID
+	 * @param {Object} [params] - Optional extra paging and sorting params, see url above
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.releases = function(user, folder, params, callback){
+		if(client.authenticated() || folder === 0){
+			var path = '/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases';
+			if((arguments.length === 3) && (typeof params === 'function')){
+				callback = params;
+			}else{
+				path = util.addParams(path, params);
+			}
+			client.get(path, callback);
+		}else{
+			callback(new Error('You must be authenticated in order to view the releases in this folder.'));
+		}
+	};
+	
+	/**
+	 * Add a release instance to the given collection folder
+	 * @param {String} user - The user name
+	 * @param {Integer} folder - The folder ID
+	 * @param {Integer} release - The release ID
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.addRelease = function(user, folder, release, callback){
+		if(client.authenticated()){
+			folder = folder||1;
+			client.post('/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases/'+release, null, callback);
+		}else{
+			callback(new Error('You must be authenticated in order to add a release to a folder.'));
+		}
+	};
+	
+	/**
+	 * Edit a release instance in the given collection folder
+	 * @param {String} user - The user name
+	 * @param {Integer} folder - The folder ID
+	 * @param {Integer} release - The release ID
+	 * @param {Instance} instance - The release instance ID
+	 * @param {Object} data - The instance data {rating: 4, folder_id: 1532}
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.editRelease = function(user, folder, release, instance, data, callback){
+		if(client.authenticated()){
+			client.post('/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases/'+release+'/instances/'+instance, data, callback);
+		}else{
+			callback(new Error('You must be authenticated in order to edit a release instance.'));
+		}
+	};
+	
+	/**
+	 * Delete a release instance from the given folder
+	 * @param {String} user - The user name
+	 * @param {Integer} folder - The folder ID
+	 * @param {Integer} release - The release ID
+	 * @param {Instance} instance - The release instance ID
+	 * @param {Function} [callback] - The callback
+	 */
+	
+	collection.removeRelease = function(user, folder, release, instance, callback){
+		if(client.authenticated()){
+			client.delete('/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases/'+release+'/instances/'+instance, callback);
+		}else{
+			callback(new Error('You must be authenticated in order to delete a release instance.'));
+		}
+	};
+	
+	return collection;
+};

lib/database.js
@@ -1,0 +1,112 @@
+var util = require('./util.js');
+
+module.exports = function(client){
+	var database = {};
+
+	/**
+	 * Expose Discogs database status constants
+	 */
+ 
+	database.status = { accepted: 'Accepted', draft: 'Draft', deleted: 'Deleted', rejected: 'Rejected' };
+	
+	/**
+	 * Get artist data from the Discogs API
+	 * 
+	 * @param {Integer} artist - The Discogs artist ID
+	 * @param {Object} [options] - Show releases by the artist + paging params
+	 * @param {Function} [callback] - Callback function
+	 */
+
+	database.artist = function(artist, callback){
+		client.get('/artists/'+artist, callback);
+	};
+
+	/**
+	 * Get artist release data from the Discogs API
+	 * 
+	 * @param {Integer} artist - The Discogs artist ID
+	 * @param {Object} [params] - Paging params
+	 * @param {Function} [callback] - Callback function
+	 */
+	database.artistReleases = function(artist, params, callback){
+		var path = '/artists/'+artist+'/releases';
+		if((arguments.length === 2) && (typeof params === 'function')){
+			callback = params;
+		}else{
+			path = util.addParams(path, params);
+		}
+		client.get(path, callback);
+	};
+
+	/**
+	 * Get release data from the Discogs API
+	 * 
+	 * @param {Integer} release - The Discogs release ID
+	 * @param {Function} [callback] - Callback function
+	 */
+	 
+	database.release = function(release, callback){
+		client.get('/releases/'+release, callback);
+	};
+
+	/**
+	 * Get master release data from the Discogs API
+	 * 
+	 * @param {Integer} master - The Discogs master release ID
+	 * @param {Function} [callback] - Callback function
+	 */
+
+	database.master = function(master, callback){
+		client.get('/masters/'+master, callback);
+	};
+
+	/**
+	 * Get the release versions contained in the given master release from the Discogs API
+	 * 
+	 * @param {Integer} master - The Discogs master release ID
+	 * @param {Object} [params] - Paging params
+	 * @param {Function} [callback] - Callback function
+	 */
+
+	database.masterVersions = function(master, params, callback){
+		var path = '/masters/'+master+'/versions';
+		if((arguments.length === 2) && (typeof params === 'function')){
+			callback = params;
+		}else{
+			path = util.addParams(path, params);
+		}
+		client.get(path, callback);
+	};
+
+	/**
+	 * Get label data from the Discogs API
+	 * 
+	 * @param {Integer} label - The Discogs label ID
+	 * @param {Function} [callback] - Callback function
+	 */
+	 
+	database.label = function(label, callback){
+		client.get('/labels/'+label, callback);
+	};
+
+	/**
+	 * Search the Discogs database
+	 * 
+	 * @param {String} query - The search query
+	 * @param {Object} [params] - Search parameters as defined on http://www.discogs.com/developers/resources/database/search-endpoint.html
+	 * @param {Function} [callback] - Callback function
+	 */
+
+	database.search = function(query, params, callback){
+		var obj = {};
+		if(arguments.length <= 2){
+			(typeof params === 'function')&&(callback = params); // No extra search params, the callback is the second function param
+		}else{
+			obj = params;
+		}
+		obj.q = util.escape(query);
+		client.get(util.addParams('/database/search', obj), callback);
+	};
+	
+	return database;
+};

lionralfs/discogs-client

lib/collection.js
@@ -1,160 +1,170 @@
-'use strict';
+var util = require('./util.js');
 
-var util = require('./util.js'),
-	AuthError = require('./error.js').AuthError;
+module.exports = function (client) {
+    var collection = {};
 
-module.exports = function(client){
-	var collection = {};
-	
-	/**
-	 * Get a list of all collection folders for the given user
-	 * @param {string} user - The user name
-	 * @param {function} callback - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.getFolders = function(user, callback){
-		return client.get('/users/'+util.escape(user)+'/collection/folders', callback);
-	};
-	
-	/**
-	 * Get metadata for a specified collection folder
-	 * @param {string} user - The Discogs user name
-	 * @param {number|string} folder - A folder ID (0 = public folder)
-	 * @param {function} callback - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.getFolder = function(user, folder, callback){
-		if(client.authenticated(2) || (parseInt(folder, 10) === 0)){
-			return client.get('/users/'+util.escape(user)+'/collection/folders/'+folder, callback);
-		}else if(typeof callback === 'function'){
-			callback(new AuthError());
-			return client;
-		}else{
-			return Promise.reject(new AuthError());
-		}
-	};
-	
-	/**
-	 * Add a new collection folder
-	 * @param {string} user - The user name
-	 * @param {string} name - The folder name
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.addFolder = function(user, name, callback){
-		return client.post({url: '/users/'+util.escape(user)+'/collection/folders', authLevel: 2}, {name: name}, callback);
-	};
-	
-	/**
-	 * Change a folder name. The name of folder 0 and 1 can't be changed.
-	 * @param {string} user - The user name
-	 * @param {(number|string)}	folder - The folder ID
-	 * @param {string} name - The new folder name
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.setFolderName = function(user, folder, name, callback){
-		return client.post({url: '/users/'+util.escape(user)+'/collection/folders/'+folder, authLevel: 2}, {name: name}, callback);
-	};
-	
-	/**
-	 * Delete a folder. A folder must be empty before it can be deleted.
-	 * @param {string} user - The user name
-	 * @param {(number|string)}	folder - The folder ID
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.deleteFolder = function(user, folder, callback){
-		return client.delete({url: '/users/'+util.escape(user)+'/collection/folders/'+folder, authLevel: 2}, callback);
-	};
-	
-	/**
-	 * Get the releases in a user's collection folder (0 = public folder)
-	 * @param {string} user - The user name
-	 * @param {(number|string)} folder - The folder ID
-	 * @param {object} [params] - Optional extra pagination and sorting params, see url above
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.getReleases = function(user, folder, params, callback){
-		if(client.authenticated(2) || (parseInt(folder, 10) === 0)){
-			var path = '/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases';
-			if((arguments.length === 3) && (typeof params === 'function')){
-				callback = params;
-			}else{
-				path = util.addParams(path, params);
-			}
-			return client.get(path, callback);
-		}else if(typeof callback === 'function'){
-			callback(new AuthError());
-			return client;
-		}else{
-			return Promise.reject(new AuthError());
-		}
-	};
-	
-	/**
-	 * Get the instances of a release in a user's collection
-	 * @param {string} user - The user name
-	 * @param {(number|string)} release - The release ID
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.getReleaseInstances = function(user, release, callback){
-		return client.get('/users/'+util.escape(user)+'/collection/releases/'+release, callback);
-	};
-	
-	/**
-	 * Add a release instance to the (optionally) given collection folder
-	 * @param {string} user - The user name
-	 * @param {(number|string)} [folder] - The folder ID (defaults to the "Uncategorized" folder)
-	 * @param {(number|string)} release - The release ID
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.addRelease = function(user, folder, release, callback){
-		if((arguments.length === 3) && (typeof release === 'function')){
-			callback = release; release = folder; folder = 1;
-		}
-		return client.post({url: '/users/'+util.escape(user)+'/collection/folders/'+(folder||1)+'/releases/'+release, authLevel: 2}, null, callback);
-	};
-	
-	/**
-	 * Edit a release instance in the given collection folder
-	 * @param {string} user - The user name
-	 * @param {(number|string)} folder - The folder ID
-	 * @param {(number|string)} release - The release ID
-	 * @param {(number|string)} instance - The release instance ID
-	 * @param {object} data - The instance data {rating: 4, folder_id: 1532}
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.editRelease = function(user, folder, release, instance, data, callback){
-		return client.post({url: '/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases/'+release+'/instances/'+instance, authLevel: 2}, data, callback);
-	};
-	
-	/**
-	 * Delete a release instance from the given folder
-	 * @param {string} user - The user name
-	 * @param {(number|string)} folder - The folder ID
-	 * @param {(number|string)} release - The release ID
-	 * @param {(number|string)} instance - The release instance ID
-	 * @param {function} [callback] - The callback
-	 * @returns {DiscogsClient|Promise}
-	 */
-	
-	collection.removeRelease = function(user, folder, release, instance, callback){
-		return client.delete({url: '/users/'+util.escape(user)+'/collection/folders/'+folder+'/releases/'+release+'/instances/'+instance, authLevel: 2}, callback);
-	};
-	
-	return collection;
+    /**
+     * Get metadata for a specified collection folder or list all collection folders
+     * @param {String} user - The user name
+     * @param {Integer} [folder] - An optional folder ID
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.folders = function (user, folder, callback) {
+        var url = '/users/' + user + '/collection/folders';
+        if (arguments.length === 2 && typeof folder === 'function') {
+            callback = folder;
+        } else {
+            url += '/' + folder;
+        }
+        client.get(url, callback);
+    };
+
+    /**
+     * Add a new collection folder
+     * @param {String} user - The user name
+     * @param {String} name - The folder name
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.addFolder = function (user, name, callback) {
+        if (client.authenticated()) {
+            client.post('/users/' + util.escape(user) + '/collection/folders', { name: name }, callback);
+        } else {
+            callback(new Error('You must be authenticated as the owner in order to add a new folder.'));
+        }
+    };
+
+    /**
+     * Change a folder name. The name of folder 0 and 1 can't be changed.
+     * @param {String} user - The user name
+     * @param {Integer}	folder - The folder ID
+     * @param {String} name - The new folder name
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.editFolder = function (user, folder, name, callback) {
+        if (client.authenticated()) {
+            client.post('/users/' + util.escape(user) + '/collection/folders/' + folder, { name: name }, callback);
+        } else {
+            callback(new Error('You must be authenticated as the owner in order to edit a folder name.'));
+        }
+    };
+
+    /**
+     * Delete a folder. A folder must be empty before it can be deleted.
+     * @param {String} user - The user name
+     * @param {Integer}	folder - The folder ID
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.deleteFolder = function (user, folder, callback) {
+        if (client.authenticated()) {
+            client.delete('/users/' + util.escape(user) + '/collection/folders/' + folder, callback);
+        } else {
+            callback(new Error('You must be authenticated as the owner in order to delete a folder.'));
+        }
+    };
+
+    /**
+     * Get the releases in a user's collection folder
+     * http://www.discogs.com/developers/resources/user/collection.html#list-releases-in-folder
+     * @param {String} user - The user name
+     * @param {Integer} folder - The folder ID
+     * @param {Object} [params] - Optional extra paging and sorting params, see url above
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.releases = function (user, folder, params, callback) {
+        if (client.authenticated() || folder === 0) {
+            var path = '/users/' + util.escape(user) + '/collection/folders/' + folder + '/releases';
+            if (arguments.length === 3 && typeof params === 'function') {
+                callback = params;
+            } else {
+                path = util.addParams(path, params);
+            }
+            client.get(path, callback);
+        } else {
+            callback(new Error('You must be authenticated in order to view the releases in this folder.'));
+        }
+    };
+
+    /**
+     * Add a release instance to the given collection folder
+     * @param {String} user - The user name
+     * @param {Integer} folder - The folder ID
+     * @param {Integer} release - The release ID
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.addRelease = function (user, folder, release, callback) {
+        if (client.authenticated()) {
+            folder = folder || 1;
+            client.post(
+                '/users/' + util.escape(user) + '/collection/folders/' + folder + '/releases/' + release,
+                null,
+                callback
+            );
+        } else {
+            callback(new Error('You must be authenticated in order to add a release to a folder.'));
+        }
+    };
+
+    /**
+     * Edit a release instance in the given collection folder
+     * @param {String} user - The user name
+     * @param {Integer} folder - The folder ID
+     * @param {Integer} release - The release ID
+     * @param {Instance} instance - The release instance ID
+     * @param {Object} data - The instance data {rating: 4, folder_id: 1532}
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.editRelease = function (user, folder, release, instance, data, callback) {
+        if (client.authenticated()) {
+            client.post(
+                '/users/' +
+                    util.escape(user) +
+                    '/collection/folders/' +
+                    folder +
+                    '/releases/' +
+                    release +
+                    '/instances/' +
+                    instance,
+                data,
+                callback
+            );
+        } else {
+            callback(new Error('You must be authenticated in order to edit a release instance.'));
+        }
+    };
+
+    /**
+     * Delete a release instance from the given folder
+     * @param {String} user - The user name
+     * @param {Integer} folder - The folder ID
+     * @param {Integer} release - The release ID
+     * @param {Instance} instance - The release instance ID
+     * @param {Function} [callback] - The callback
+     */
+
+    collection.removeRelease = function (user, folder, release, instance, callback) {
+        if (client.authenticated()) {
+            client.delete(
+                '/users/' +
+                    util.escape(user) +
+                    '/collection/folders/' +
+                    folder +
+                    '/releases/' +
+                    release +
+                    '/instances/' +
+                    instance,
+                callback
+            );
+        } else {
+            callback(new Error('You must be authenticated in order to delete a release instance.'));
+        }
+    };
+
+    return collection;
 };

lib/database.js
@@ -1,178 +1,111 @@
-'use strict';
-
 var util = require('./util.js');
 
-module.exports = function(client) {
+module.exports = function (client) {
     var database = {};
 
     /**
      * Expose Discogs database status constants
      */
 
-    database.status = {accepted: 'Accepted', draft: 'Draft', deleted: 'Deleted', rejected: 'Rejected'};
+    database.status = { accepted: 'Accepted', draft: 'Draft', deleted: 'Deleted', rejected: 'Rejected' };
 
     /**
-     * Get artist data
-     * @param {(number|string)} artist - The Discogs artist ID
-     * @param {object} [options] - Show releases by the artist + pagination params
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
+     * Get artist data from the Discogs API
+     *
+     * @param {Integer} artist - The Discogs artist ID
+     * @param {Object} [options] - Show releases by the artist + paging params
+     * @param {Function} [callback] - Callback function
      */
 
-    database.getArtist = function(artist, callback) {
-        return client.get('/artists/' + artist, callback);
+    database.artist = function (artist, callback) {
+        client.get('/artists/' + artist, callback);
     };
 
     /**
-     * Get artist release data
-     * @param {(number|string)} artist - The Discogs artist ID
-     * @param {object} [params] - Optional pagination params
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
+     * Get artist release data from the Discogs API
+     *
+     * @param {Integer} artist - The Discogs artist ID
+     * @param {Object} [params] - Paging params
+     * @param {Function} [callback] - Callback function
      */
-
-    database.getArtistReleases = function(artist, params, callback) {
+    database.artistReleases = function (artist, params, callback) {
         var path = '/artists/' + artist + '/releases';
-        if ((arguments.length === 2) && (typeof params === 'function')) {
+        if (arguments.length === 2 && typeof params === 'function') {
             callback = params;
         } else {
             path = util.addParams(path, params);
         }
-        return client.get(path, callback);
+        client.get(path, callback);
     };
 
     /**
-     * Get release data
-     * @param {(number|string)} release - The Discogs release ID
-     * @param {function} [callback] - Callback 
-     * @return {DiscogsClient|Promise}
+     * Get release data from the Discogs API
+     *
+     * @param {Integer} release - The Discogs release ID
+     * @param {Function} [callback] - Callback function
      */
 
-    database.getRelease = function(release, callback) {
-        return client.get('/releases/' + release, callback);
+    database.release = function (release, callback) {
+        client.get('/releases/' + release, callback);
     };
 
     /**
-     * Get the release rating for the given user
-     * @param {(number|string)} release - The Discogs release ID
-     * @param {string} user - The Discogs user name
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
+     * Get master release data from the Discogs API
+     *
+     * @param {Integer} master - The Discogs master release ID
+     * @param {Function} [callback] - Callback function
      */
 
-    database.getReleaseRating = function(release, user, callback) {
-        return client.get('/releases/' + release + '/rating/' + util.escape(user), callback);
+    database.master = function (master, callback) {
+        client.get('/masters/' + master, callback);
     };
 
     /**
-     * Set (or remove) a release rating for the given logged in user
-     * @param {(number|string)} release - The Discogs release ID
-     * @param {string} user - The Discogs user name
-     * @param {number} rating - The new rating for a release between 1 and 5. Null = remove rating
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
+     * Get the release versions contained in the given master release from the Discogs API
+     *
+     * @param {Integer} master - The Discogs master release ID
+     * @param {Object} [params] - Paging params
+     * @param {Function} [callback] - Callback function
      */
 
-    database.setReleaseRating = function(release, user, rating, callback) {
-        var url = '/releases/' + release + '/rating/' + util.escape(user);
-        if (!rating) {
-            return client.delete({url: url, authLevel: 2}, callback);
-        } else {
-            return client.put({url: url, authLevel: 2}, {rating: ((rating > 5) ? 5 : rating)}, callback);
-        }
-    };
-
-    /**
-     * Get master release data
-     * @param {(number|string)} master - The Discogs master release ID
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
-     */
-
-    database.getMaster = function(master, callback) {
-        return client.get('/masters/' + master, callback);
-    };
-
-    /**
-     * Get the release versions contained in the given master release
-     * @param {(number|string)} master - The Discogs master release ID
-     * @param {object} [params] - optional pagination params
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
-     */
-
-    database.getMasterVersions = function(master, params, callback) {
+    database.masterVersions = function (master, params, callback) {
         var path = '/masters/' + master + '/versions';
-        if ((arguments.length === 2) && (typeof params === 'function')) {
+        if (arguments.length === 2 && typeof params === 'function') {
             callback = params;
         } else {
             path = util.addParams(path, params);
         }
-        return client.get(path, callback);
+        client.get(path, callback);
     };
 
     /**
-     * Get label data
-     * @param {(number|string)} label - The Discogs label ID
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
+     * Get label data from the Discogs API
+     *
+     * @param {Integer} label - The Discogs label ID
+     * @param {Function} [callback] - Callback function
      */
 
-    database.getLabel = function(label, callback) {
-        return client.get('/labels/' + label, callback);
+    database.label = function (label, callback) {
+        client.get('/labels/' + label, callback);
     };
 
     /**
-     * Get label release data
-     * @param {(number|string)} label - The Discogs label ID
-     * @param {object} [params] - Optional pagination params
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
+     * Search the Discogs database
+     *
+     * @param {String} query - The search query
+     * @param {Object} [params] - Search parameters as defined on http://www.discogs.com/developers/resources/database/search-endpoint.html
+     * @param {Function} [callback] - Callback function
      */
 
-    database.getLabelReleases = function(label, params, callback) {
-        var path = '/labels/' + label + '/releases';
-        if ((arguments.length === 2) && (typeof params === 'function')) {
-            callback = params;
-        } else {
-            path = util.addParams(path, params);
-        }
-        return client.get(path, callback);
-    };
-
-    /**
-     * Get an image
-     * @param {string} url - The full image url
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
-     */
-
-    database.getImage = function(url, callback) {
-        return client.get({url: url, encoding: 'binary', queue: false, json: false}, callback);
-    };
-
-    /**
-     * Search the database
-     * @param {string} query - The search query
-     * @param {object} [params] - Search parameters as defined on http://www.discogs.com/developers/#page:database,header:database-search
-     * @param {function} [callback] - Callback function
-     * @return {DiscogsClient|Promise}
-     */
-
-    database.search = function(query, params, callback) {
+    database.search = function (query, params, callback) {
         var obj = {};
-        if ((arguments.length === 2) && (typeof params === 'function')) {
-            callback = params;
-        }
-        if (typeof params === 'object') {
+        if (arguments.length <= 2) {
+            typeof params === 'function' && (callback = params); // No extra search params, the callback is the second function param
+        } else {
             obj = params;
-        } else if (typeof query === 'object') {
-            obj = query;
         }
-        if (typeof query === 'string') {
-            obj.q = query;
-        }
-        return client.get({url: util.addParams('/database/search', obj), authLevel: 1}, callback);
+        obj.q = util.escape(query);
+        client.get(util.addParams('/database/search', obj), callback);
     };
 
     return database;