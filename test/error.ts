d2270a32c3eba6cf6a04bd5bccfbe7d991b134bb
Lion Ralfs <lion.ralfs@gmail.com>
2022-04-24 13:32:39 +0200

    feat(test/errors): check if API error is passed to consumer

--- test/error.ts
+++ test/error.ts
@@ -1,5 +1,10 @@
 import test from 'ava';
+import { rest } from 'msw';
+import { DiscogsClient } from '../lib/client.js';
 import { DiscogsError, AuthError } from '../lib/error.js';
+import { setupMockAPI } from './_setup.js';
+
+const server = setupMockAPI();
 
 test('Error: Test DiscogsError', t => {
     let discogsError = new DiscogsError(403, 'Test');
@@ -13,4 +18,24 @@ test('Error: Test AuthError', t => {
     // t.true(authError instanceof AuthError, 'Instance of AuthError');
     t.true(authError instanceof Error, 'Instance of Error');
     t.is(authError.statusCode, 401, 'Status code === 401');
+});
+
+test.serial('Error: Passed an instance of DiscogsError when bad status code', async t => {
+    t.plan(4);
+
+    server.use(
+        rest.get('https://api.discogs.com/labels/1123123123123/releases', (req, res, ctx) => {
+            t.pass();
+            return res(ctx.status(404), ctx.json({ message: 'error message' }));
+        })
+    );
+
+    let client = new DiscogsClient();
+    try {
+        await client.database().getLabelReleases(1123123123123, { page: 3, per_page: 25 });
+    } catch (err: any) {
+        t.is(err.statusCode, 404);
+        t.is(err.message, 'error message');
+        t.true(err instanceof DiscogsError);
+    }
 });
