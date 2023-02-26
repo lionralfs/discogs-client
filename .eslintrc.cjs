b3ab7c3418dd27d06c21889f0e4c72667001f82e
Lion Ralfs <lion.ralfs@gmail.com>
2023-02-26 14:29:09 +0100

    chore(lint): enforce consistent type imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,7 @@ module.exports = {
         sourceType: 'module',
     },
     plugins: ['@typescript-eslint'],
-    rules: {},
+    rules: {
+        '@typescript-eslint/consistent-type-imports': 'error',
+    },
 };
