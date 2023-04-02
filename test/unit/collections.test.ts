05709a7089194fb24b51bdd0174316ee00c0037a
Lion Ralfs <lion.ralfs@gmail.com>
2023-04-02 16:51:30 +0200

    use mocks in tests

--- test/unit/collections.test.ts
+++ test/unit/collections.test.ts
@@ -1,14 +1,24 @@
 import test from 'ava';
-import { DiscogsClient } from '@lib/client.js';
+import collectionFactory from '@lib/collection.js';
+import type { DiscogsClient } from '@lib/client.js';
+import { Substitute } from '@fluffy-spoon/substitute';
 
-test('Collection: Get folder metadata (throws auth error)', async t => {
-    const client = new DiscogsClient();
-    await t.throwsAsync(client.user().collection().getFolder('rodneyfool', 1234));
+test('Collection: Get folder metadata (throws auth error because not authenticated and requesting folder_id != 0)', async t => {
+    // Given
+    const client = Substitute.for<DiscogsClient>();
+    const collection = collectionFactory(client);
+    client.authenticated(2).returns(false);
+
+    // When/Then
+    await t.throwsAsync(collection.getFolder('rodneyfool', 1234));
 });
 
 test('Collection: Collection items by folder (throws auth error)', async t => {
-    const client = new DiscogsClient();
-    await t.throwsAsync(
-        client.user().collection().getReleases('rodneyfool', '1234', { sort: 'artist', sort_order: 'desc' })
-    );
+    // Given
+    const client = Substitute.for<DiscogsClient>();
+    const collection = collectionFactory(client);
+    client.authenticated(2).returns(false);
+
+    // When/Then
+    await t.throwsAsync(collection.getReleases('rodneyfool', '1234', { sort: 'artist', sort_order: 'desc' }));
 });
